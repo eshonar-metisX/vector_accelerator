fed0b9e1f81b2be5b6ce1ecde0a089cf68adcf0e
chpark <eshonar@metisx.com>
2023-10-25 18:35:52 +0900

    updated cmakelist for gtest link

---
+++ test/build/CMakeFiles/test.exec.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.exec.dir/test_main.cpp.o"
+  "CMakeFiles/test.exec.dir/test_main.cpp.o.d"
+  "test.exec"
+  "test.exec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.exec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test/build/CMakeFiles/test.exec.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for test.exec.
+# This may be replaced when dependencies are built.
