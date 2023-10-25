89bb6538b8daf1efa838b5168d9bae2ebb4de446
chpark <eshonar@metisx.com>
2023-10-25 16:01:35 +0900

    class name changed to camel from snake

--- test/test_cases.hpp
+++ test/test_cases.hpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include "VectorAcceleratorTester.hpp"
-#include "../inc/vector_accelerator.hpp"
+#include "../inc/vectorAccelerator.hpp"
 
 static const uint64_t testSize = 100;
 
@@ -8,46 +8,46 @@ TEST_F(VectorAcceleratorTester, TestCase_ADD_INT32)
 {    
     ConfigureTest(TestCase::ADD, TestDataType::INT32, testSize);
     SetUpArr(100, 200);
-    EXPECT_TRUE(IsEqArr(metisx::vector_accelerator::add(intLhs, intRhs, intRes, testSize), 300, testSize));
+    EXPECT_TRUE(IsEqArr(metisx::vectorAccelerator::add(intLhs, intRhs, intRes, testSize), 300, testSize));
 }
 
 TEST_F(VectorAcceleratorTester, TestCase_ADD_INT32_OVERFLOW)
 {
     ConfigureTest(TestCase::ADD, TestDataType::INT32, testSize);
     SetUpArr(100, 200);
-    EXPECT_TRUE(IsEqArr(metisx::vector_accelerator::add(intLhs, intRhs, intRes, testSize), 300, testSize));
+    EXPECT_TRUE(IsEqArr(metisx::vectorAccelerator::add(intLhs, intRhs, intRes, testSize), 300, testSize));
 }
 
 
 
 // TEST_F(VectorAcceleratorTester, TestCase_ADD_INT64)
 // {
-//    EXPECT_TRUE(IsEqArr(metisx::vector_accelerator::add(int_arr_lhs, int_arr_rhs, int_arr_res, testSize), 3));
+//    EXPECT_TRUE(IsEqArr(metisx::vectorAccelerator::add(int_arr_lhs, int_arr_rhs, int_arr_res, testSize), 3));
 // }
 
 // TEST_F(VectorAcceleratorTester, TestCase_ADD_INT64_OVERFLOW)
 // {
-//    EXPECT_TRUE(IsEqArr(metisx::vector_accelerator::add(int_arr_lhs, int_arr_rhs, int_arr_res, testSize), 3));
+//    EXPECT_TRUE(IsEqArr(metisx::vectorAccelerator::add(int_arr_lhs, int_arr_rhs, int_arr_res, testSize), 3));
 // }
 
 // TEST_F(VectorAcceleratorTester, TestCase_ADD_UINT32)
 // {
-//    EXPECT_TRUE(IsEqArr(metisx::vector_accelerator::add(int_arr_lhs, int_arr_rhs, int_arr_res, testSize), 3));
+//    EXPECT_TRUE(IsEqArr(metisx::vectorAccelerator::add(int_arr_lhs, int_arr_rhs, int_arr_res, testSize), 3));
 // }
 
 // TEST_F(VectorAcceleratorTester, TestCase_ADD_UINT32_OVERFLOW)
 // {
-//    EXPECT_TRUE(IsEqArr(metisx::vector_accelerator::add(int_arr_lhs, int_arr_rhs, int_arr_res, testSize), 3));
+//    EXPECT_TRUE(IsEqArr(metisx::vectorAccelerator::add(int_arr_lhs, int_arr_rhs, int_arr_res, testSize), 3));
 // }
 
 // TEST_F(VectorAcceleratorTester, TestCase_ADD_UINT64)
 // {
-//    EXPECT_TRUE(IsEqArr(metisx::vector_accelerator::add(int_arr_lhs, int_arr_rhs, int_arr_res, testSize), 3));
+//    EXPECT_TRUE(IsEqArr(metisx::vectorAccelerator::add(int_arr_lhs, int_arr_rhs, int_arr_res, testSize), 3));
 // }
 
 // TEST_F(VectorAcceleratorTester, TestCase_ADD_UINT64_OVERFLOW)
 // {
-//    EXPECT_TRUE(IsEqArr(metisx::vector_accelerator::add(int_arr_lhs, int_arr_rhs, int_arr_res, testSize), 3));
+//    EXPECT_TRUE(IsEqArr(metisx::vectorAccelerator::add(int_arr_lhs, int_arr_rhs, int_arr_res, testSize), 3));
 // }
 
 
@@ -59,7 +59,7 @@ TEST_F(VectorAcceleratorTester, TestCase_ADD_INT32_OVERFLOW)
 //
 //TEST_F(VectorAcceleratorTester, TestCase_SUB)
 //{
-//   EXPECT_TRUE(isEqArr(-1, metisx::vector_accelerator(1) - metisx::vector(2)));
+//   EXPECT_TRUE(isEqArr(-1, metisx::vectorAccelerator(1) - metisx::vector(2)));
 //}
 //
 //TEST_F(VectorAcceleratorTester, TestCase_SUB)
